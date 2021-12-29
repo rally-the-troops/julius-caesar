62b1b620c7cad746aa9a13978ddb68917b97df97
Tor Andersson <tor@ccxvii.net>
2021-12-29 17:01:28 +0100

    Simplify styles and html for play.html

---
+++ title.sql
@@ -0,0 +1,3 @@
+insert or replace into titles values ( 'julius-caesar', 'Julius Caesar', 37836 );
+insert or replace into roles values ( 'julius-caesar', 'Caesar' );
+insert or replace into roles values ( 'julius-caesar', 'Pompeius' );
